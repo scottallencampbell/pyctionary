a0abe7d4e5d0bde74d5ed0612f20d57c092ace60
Scott Campbell <scampbell@hammerbeam.com>
2025-12-03 15:28:35 -0600

    Refactor deployment configuration and improve performance optimizations

--- ml/test.py
+++ ml/test.py
@@ -4,9 +4,10 @@ from skimage.io import imread, imshow
 import keras.api._v2.keras as keras
 from matplotlib import pyplot as plt
 
-PREDICTION_IMAGES_PATH  = './images/predict/'
-MODEL_PATH = './models/model.hdf5'
+PREDICTION_IMAGES_PATH = "./images/predict/"
+MODEL_PATH = "./models/model.hdf5"
 DIGITS = list(range(0, 10))
+
 
 def load_images(path):
     images = []
@@ -18,24 +19,24 @@ def load_images(path):
         image = image / 255
 
         images.append(image)
-        labels.append(filename[0])       
+        labels.append(filename[0])
 
     np.expand_dims(images, axis=3)
 
     return np.array(images), np.array(labels)
 
-    
+
 model = keras.models.load_model(MODEL_PATH)
 
 predicition_images, prediction_labels = load_images(PREDICTION_IMAGES_PATH)
 predictions = model.predict(predicition_images, verbose=0)
 
 for i, prediction in enumerate(predictions):
-    print('\r\n----> ' + prediction_labels[i])    
+    print("\r\n----> " + prediction_labels[i])
     z = list(zip(prediction, DIGITS))
-    
+
     sorted_prediction = sorted(z, reverse=True)
 
     for item in sorted_prediction:
-        if (item[0] > 0.01):
-            print(f'{item[1]}: {round(item[0]*100)}%')
+        if item[0] > 0.01:
+            print(f"{item[1]}: {round(item[0] * 100)}%")
